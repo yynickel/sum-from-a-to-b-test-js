ca4f95685f0f6f7afcc5b1f0cd4498ae5c24a271
Nick Yu <yynickel@gmail.com>
2019-06-28 02:49:58 +0000

    recursion added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (toN !== fromN) {
+    return toN + sum(fromN, toN - 1);
+  } else {
+    return fromN;
+  }
 }
 
 module.exports = sum;
